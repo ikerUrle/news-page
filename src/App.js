ec79cecbb322a21dca9d060cfc9a715047c0e87f
ikerUrle <ikerurlezaga@gmail.com>
2020-09-01 14:03:54 +0200

    Search bar animation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import Table from "./components/Table";
 import "./App.scss";
 import Sidebar from "./components/Sidebar";
+import Searchbar from "./components/Searchbar";
 
 const App = () => {
   const options = ["posts", "comments", "todos"];
   const [articles, setArticles] = useState([]);
   const [selectedOption, setSelectedOption] = useState("posts");
+  const [query, setQuery] = useState("");
 
   let url = `https://jsonplaceholder.typicode.com/${selectedOption}`;
 
@@ -18,12 +20,15 @@ const App = () => {
 
   return (
     <div className="app">
-      <Sidebar
-        options={options}
-        selectedOption={selectedOption}
-        setSelectedOption={setSelectedOption}
-      />
-      <Table articles={articles} />
+      <Searchbar query={query} setQuery={setQuery} />
+      <div className="app-body">
+        <Sidebar
+          options={options}
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+        />
+        <Table articles={articles} />
+      </div>
     </div>
   );
 };
---
+++ src/components/Searchbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Searchbar.scss";
+
+const Searchbar = ({ query, setQuery }) => {
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const focusSearch = () => {
+    console.log("hover");
+    document.getElementsByClassName("search-input")[0].focus();
+  };
+  return (
+    <div className="search">
+      <button className="search-button" onMouseEnter={focusSearch}>
+        <i class="fas fa-search"></i>
+      </button>
+      <input
+        className="search-input"
+        placeholder="Sports, entertainment, politics..."
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Searchbar;
