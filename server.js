d3348d8b8d49330d98ffc7f304d016b873153e0d
ikerUrle <ikerurlezaga@gmail.com>
2020-09-02 16:43:25 +0200

    Fixing Proxy

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("build"));
 
-const apiProxy = createProxyMiddleware("/api", {
-  target: "https://newsapi.org/v2",
-});
-
-app.use(apiProxy);
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://newsapi.org/v2",
+    changeOrigin: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Website serving on port ${PORT}...`);
