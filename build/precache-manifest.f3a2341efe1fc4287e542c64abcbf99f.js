80f090cc10dc2379801f7f9fabe5785baafc663c
ikerUrle <ikerurlezaga@gmail.com>
2020-09-03 11:02:50 +0200

    Deploy to github

---
+++ build/precache-manifest.f3a2341efe1fc4287e542c64abcbf99f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3da7b45808e8d80a2e94dd14c85637c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eab97cace3f484e1eaa",
+    "url": "/static/css/main.ef59a3eb.chunk.css"
+  },
+  {
+    "revision": "1b482b81bd65ad8fa491",
+    "url": "/static/js/2.389d65b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.389d65b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eab97cace3f484e1eaa",
+    "url": "/static/js/main.3ac65ef4.chunk.js"
+  },
+  {
+    "revision": "f8e19c8708b89a2b2fa9",
+    "url": "/static/js/runtime-main.92f30bd9.js"
+  },
+  {
+    "revision": "f8e58ef224fc5fe8e71e17651a6c250e",
+    "url": "/static/media/news-photo.f8e58ef2.png"
+  },
+  {
+    "revision": "74abfc2d6f334003a71ec0e85b95f328",
+    "url": "/static/media/newspaper.74abfc2d.svg"
+  },
+  {
+    "revision": "0f7fe590b33417e32dab43b8ab53217f",
+    "url": "/static/media/no-news.0f7fe590.svg"
+  }
+]);
